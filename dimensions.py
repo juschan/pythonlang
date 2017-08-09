c81ecadaa8a7b06ca4e92ce19eae1f229c5f8c62
Justin Chan <jcsl76@yahoo.com>
2017-08-09 16:59:15 +0100

    Added dimensions.py for tuples - immutable lists

---
+++ dimensions.py
@@ -0,0 +1,15 @@
+#tuples - immutable lists
+dimensions = (200, 50)
+print(dimensions[0])
+
+#error if you try to modify
+#dimensions[0] = 250
+
+#loops
+for dim in dimensions:
+	print(dim)
+
+
+#you can redefine the entire tuple
+dimensions = (4, 6)
+print(dimensions)
