5d788e38c2ece9472fac20345e987d98569f9510
Justin Chan <jcsl76@yahoo.com>
2017-10-31 15:53:07 +0000

    Modified die_visual.py to add x-axis labels. svg not updated.

--- data_viz/die_visual.py
+++ data_viz/die_visual.py
@@ -20,6 +20,7 @@ for value in range(1, die.num_sides+1):
 hist=pygal.Bar()
 hist.title="Results of rolling one D6 1000 times"
 hist.x_title="Result"
-hist.y_title="Frequence of Result"
+hist.y_title="Frequency of Result"
 hist.add('D6', freq)
+hist.x_labels = map(str, range(1, 6))
 hist.render_to_file('die_visual.svg')
